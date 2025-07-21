d197ecda3f152cd8aec2229596e2c608e8176897
Bach Pham <khoabach2233@gmail.com>
2025-07-21 16:27:38 +0700

    update: Add template main version 1.0

---
+++ main_v1.0.c
@@ -0,0 +1,42 @@
+/**
+ * @file       cbuffer.c
+ * @copyright  
+ * @license    
+ * @version    1.0.0
+ * @date       2025-07-21
+ * @author     Bach Pham
+ * @author     Chinh Nguyen
+ * @author     Tai Tran
+ * @author     Phuong Mai   
+ * @brief      This file include the init function and clear function for cbuffer
+ *
+ * @note       None
+ * @example    cbuffer_t cb;
+ *             uint8_t cb_buff[6];
+ *             uint8_t a;
+ *             void main(void)
+ *             {
+ *                 cb_init(&cb, cb_buff, 100);
+ *                 cb_clear(&cb);
+ *             }
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "cbuffer.h"
+//#include "code_template.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+/* Private defines ---------------------------------------------------- */
+/* Private enumerate/structure ---------------------------------------- */
+/* Private macros ----------------------------------------------------- */
+/* Public variables --------------------------------------------------- */
+/* Private variables -------------------------------------------------- */
+/* Private function prototypes ---------------------------------------- */
+/* Function definitions ----------------------------------------------- */
+
+/* Private definitions ----------------------------------------------- */
+/* End of file -------------------------------------------------------- */
